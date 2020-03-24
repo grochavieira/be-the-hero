cd9ce348127e371b76e4d17c3a6c8ceedc8de367
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-03-24 11:34:20 -0300

    backend completed

---
+++ backend/src/database/migrations/20200324101703_create_ongs.js
@@ -0,0 +1,16 @@
+// Responsável pela criação da tabela
+exports.up = function(knex) {
+  return knex.schema.createTable("ongs", function(table) {
+    table.string("id").primary();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    table.string("whatsapp").notNullable();
+    table.string("city").notNullable();
+    table.string("uf", 2).notNullable();
+  });
+};
+
+// Quando acontece algum erro
+exports.down = function(knex) {
+  return knex.schema.dropTable("ongs");
+};
